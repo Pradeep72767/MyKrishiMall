5e712f25838588661d3b1afc4aec041d88b461f3
Pradeep72767 <pradeep250598@gmail.com>
2021-05-10 16:39:58 +0530

    review of products

--- app/src/main/java/com/example/mykrishimall/CheckNewProductsUploadByFarmesActivity.java
+++ app/src/main/java/com/example/mykrishimall/CheckNewProductsUploadByFarmesActivity.java
@@ -1,24 +1,86 @@
 package com.example.mykrishimall;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.mykrishimall.Model.Products;
+import com.example.mykrishimall.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class CheckNewProductsUploadByFarmesActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
 
+    TextView txt1, txt2, txt3;
+
+    private DatabaseReference adminProductRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_new_products_upload_by_farmes);
 
+        adminProductRef = FirebaseDatabase.getInstance().getReference().child("Crops");
+
         recyclerView = findViewById(R.id.admin_new_product_list);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
+
     }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Products> options =
+                new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(adminProductRef, Products.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Products model)
+                    {
+                        holder.txtProductName.setText(model.getName());
+                        holder.txtProductDesc.setText(model.getDescription());
+                        holder.txtProductPrice.setText("Price = " + model.getPrice() + "Rs.");
+
+                        Picasso.get().load(model.getImage()).into(holder.imageView);
+
+                    }
+
+                    @NonNull
+                    @Override
+                    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+                    {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_item_layout, parent, false);
+                        ProductViewHolder holder = new ProductViewHolder(view);
+
+                        return holder;
+
+                    }
+                };
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+
+    }
+
+
 }
